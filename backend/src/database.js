ced1a728b587176bd82a49886d0d6227ef06dfaa
Rouxlss <arnol.ortizjs@gmail.com>
2021-08-29 21:05:55 -0600

    configs

--- backend/src/database.js
+++ backend/src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const URI = process.env.MONGODB_URI
     ? process.env.MONGODB_URI
-    : 'mongodb://localhost/databasetest';
+    : 'mongodb+srv://newuser:password12345@library.e7whf.mongodb.net/library?retryWrites=true&w=majority';
 
 console.log(URI)
 
